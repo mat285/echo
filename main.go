8fb0f7438d7a67f7997dfce814c81c34e2e8bd2a
michaelt <michaelt@blend.com>
2017-09-20 12:38:55 -0700

    fail

--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 )
 
 func main() {
-	fail := len(os.Args) > 1
 
 	num := 30
 
 	for i := 0; i < num; i++ {
 		fmt.Println(i)
 		time.Sleep(time.Second)
-		if i > num/2 && fail {
+		if i > num/2 {
 			panic("failing now")
 		}
 	}
