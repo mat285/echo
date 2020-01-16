45b118f7543216750953db2abd0c66871cc0108c
mat285 <mat285@cornell.edu>
2020-01-16 11:42:44 -0500

    main change

--- main.go
+++ main.go
@@ -107,9 +107,16 @@ func ping() {
 			fmt.Println(err.Error())
 			continue
 		}
-		_, err = http.DefaultClient.Do(req)
+		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			fmt.Println(err.Error())
+			continue
 		}
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		fmt.Println(string(data))
 	}
 }
